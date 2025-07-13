1cb594995982affbac125ba006f9b3c6c57b537e
Captaindaddy77 <nabilzaky77@gmail.com>
2025-07-13 17:46:11 +0800

    encapsulation, constructor, mutator, accessor

--- src/Parfum.java
+++ src/Parfum.java
@@ -1,6 +1,47 @@
 public class Parfum {
-        // Atribut
+    // Atribut
     private String nama;
     private double harga;
     private String jenis;
-}
+    
+    // Konstruktor
+    public Parfum(String nama, double harga, String jenis) {
+        this.nama = nama;
+        this.harga = harga;
+        this.jenis = jenis;
+    }
+    
+    // Mutator
+    public String getNama() {
+        return nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public String getJenis() {
+        return jenis;
+    }
+    
+    // Accessor
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }  
+    
+    // Menampilkan informasi parfum
+    public void tampilInfo() {
+        System.out.println("Nama Parfum: " + nama);
+        System.out.println("Harga: Rp " + harga);
+        System.out.println("Jenis: " + jenis);
+    }
+}   
+
